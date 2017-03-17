52d4a6aa1db4c02d4d806c65557fb56c097b280b
Antony West <antonywest.design@gmail.com>
2017-03-17 07:54:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 function dwarfRollCall(dwarves) {
+  var dwarveLineUp = "";
+  for (var i = 0; i < dwarves.length; i++) {
+    dwarveLineUp += (i+1).toString() + ". " + dwarves[i] + " ";
+  }
+  return dwarveLineUp;
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var planteerYell = [];
+  for (var i = 0; i < planeteerCalls.length; i++) {
+   planteerYell.push(planeteerCalls[i].toUpperCase() + "!");
+  }
+  return planteerYell;
 }
 
 function longPlaneteerCalls(words) {
+  var findLong = false;
+  for (var i = 0; i < words.length; i++) {
+    if (words[i].length > 4) {
+      findLong = true;
+    }
+  }
+  return findLong;
 }
 
 function findTheCheese (foods) {
+  var fancyCheese = ["cheddar", "gouda", "camembert"];
+    for (var i = 0; i < foods.length; i++) {
+      for (var j = 0; j < fancyCheese.length; j++){
+        if (fancyCheese[j] === foods[i]) {
+          return foods[i];
+        }
+      }
+    }
+    return "no cheese!";
 }
